a2246dbe5f6c0c4239035241fee1dd1c949271fc
Michail Lukov <michail@23snaps.com>
2018-08-01 22:13:45 +0300

    no message

---
+++ XML_Validator/MainForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace XML_Validator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.txtXsdSchema = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.btnParseXsd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnChooseXsd = new System.Windows.Forms.Button();
+            this.txtXsdFileName = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            resources.ApplyResources(this.splitContainer1, "splitContainer1");
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.txtXsdSchema);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.txtResult);
+            // 
+            // txtXsdSchema
+            // 
+            resources.ApplyResources(this.txtXsdSchema, "txtXsdSchema");
+            this.txtXsdSchema.Name = "txtXsdSchema";
+            // 
+            // txtResult
+            // 
+            resources.ApplyResources(this.txtResult, "txtResult");
+            this.txtResult.Name = "txtResult";
+            // 
+            // splitContainer2
+            // 
+            resources.ApplyResources(this.splitContainer2, "splitContainer2");
+            this.splitContainer2.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.txtXsdFileName);
+            this.splitContainer2.Panel1.Controls.Add(this.btnChooseXsd);
+            this.splitContainer2.Panel1.Controls.Add(this.label2);
+            this.splitContainer2.Panel1.Controls.Add(this.label1);
+            this.splitContainer2.Panel1.Controls.Add(this.btnParseXsd);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.splitContainer1);
+            // 
+            // btnParseXsd
+            // 
+            resources.ApplyResources(this.btnParseXsd, "btnParseXsd");
+            this.btnParseXsd.Name = "btnParseXsd";
+            this.btnParseXsd.UseVisualStyleBackColor = true;
+            this.btnParseXsd.Click += new System.EventHandler(this.btnParseXsd_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // btnChooseXsd
+            // 
+            resources.ApplyResources(this.btnChooseXsd, "btnChooseXsd");
+            this.btnChooseXsd.Name = "btnChooseXsd";
+            this.btnChooseXsd.UseVisualStyleBackColor = true;
+            this.btnChooseXsd.Click += new System.EventHandler(this.btnChooseXsd_Click);
+            // 
+            // txtXsdFileName
+            // 
+            resources.ApplyResources(this.txtXsdFileName, "txtXsdFileName");
+            this.txtXsdFileName.Name = "txtXsdFileName";
+            // 
+            // MainForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.splitContainer2);
+            this.Name = "MainForm";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnParseXsd;
+        private System.Windows.Forms.TextBox txtXsdSchema;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.TextBox txtXsdFileName;
+        private System.Windows.Forms.Button btnChooseXsd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ XML_Validator/MainForm.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Schema;
+using System.IO;
+
+namespace XML_Validator
+{
+    public partial class MainForm : Form
+    {
+       
+        static int errorsCount = 0;
+        static string errorsText = "";
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnParseXsd_Click( object sender, EventArgs e )
+        {
+            string strResult = "";
+            txtResult.Text = "";
+
+            if( txtXsdFileName.Text == "" )
+            {
+                MessageBox.Show( "Please choose Xsd file to validate against!" );
+                txtXsdFileName.Focus();
+                return;
+            }// if
+
+            if( txtXsdSchema.Text == "" )
+            {
+                MessageBox.Show( "Please enter XML text to validate!" );
+                txtXsdSchema.Focus();
+                return;
+            }// if
+
+            if( !File.Exists( txtXsdFileName.Text ) )
+            {
+                MessageBox.Show( "Can't find XSD file! File: " + txtXsdFileName.Text );
+                return;
+            }// if
+
+
+            if( !ValidateXmlBySchema( txtXsdSchema.Text, txtXsdFileName.Text, ref strResult ) )
+            {
+                strResult += errorsText;
+                txtResult.Text = "Validation Failed! :" + strResult;
+                return;
+            }
+
+
+            txtResult.Text = "Validation successful.";
+        }
+
+        private void btnChooseXsd_Click( object sender, EventArgs e )
+        {
+            txtXsdFileName.Text = "";
+
+            OpenFileDialog openFileDlg = new OpenFileDialog();
+            openFileDlg.Filter = "Xsd files (*.xsd)|*.xsd|All files (*.*)|*.*";
+
+            openFileDlg.Title = "Please select an image Xsd fiel to vaidate against.";
+            if( openFileDlg.ShowDialog() == DialogResult.OK )
+            {
+                txtXsdFileName.Text = openFileDlg.FileName;
+            }// if
+        }
+
+
+        public bool ValidateXmlBySchema( string xmlInput, string schemaFileName, ref string errorMessage )
+        {
+            errorsCount = 0;
+            errorMessage = "";
+            errorsText = "";
+            try
+            {
+                XmlReaderSettings settings = new XmlReaderSettings();
+                settings.ValidationType = ValidationType.Schema;
+                settings.ValidationFlags |= XmlSchemaValidationFlags.ProcessInlineSchema;
+                settings.ValidationFlags |= XmlSchemaValidationFlags.ProcessSchemaLocation;
+                settings.ValidationFlags |= XmlSchemaValidationFlags.ReportValidationWarnings;
+                settings.ValidationEventHandler += new ValidationEventHandler( UpdateRatingsValidationHandler );
+
+                string schemaFullFileName = schemaFileName;
+
+                settings.Schemas.Add( null, schemaFullFileName );
+
+                XmlReader reader = XmlReader.Create( new System.IO.StringReader( xmlInput ),
+                                                        settings );
+                // Parse the file. 
+                while( reader.Read() ) ;
+
+                reader.Close();
+
+            }//try
+            catch( Exception error )
+            {
+                // XML Validation failed
+                errorMessage = string.Concat( "XML validation failed.\r\nError Message: ", error.Message );
+                return false;
+            }// catch
+
+            return errorsCount == 0;
+        }
+
+        public static void UpdateRatingsValidationHandler( object sender, ValidationEventArgs args )
+        {
+            errorsCount++;
+            errorsText = string.Concat( errorsText,  args.Message + "\r\n"  );
+        }
+
+    }
+}
